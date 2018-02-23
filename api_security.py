977f3e1bf3cea1b3c156a3cee21a083faf83316e
SrivaniMalladi <srivanigorti@gmail.com>
2018-02-23 12:45:44 +0530

    api to post data into kinesis stream

---
+++ api_security.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+import os
+import boto3
+import json
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_kinesis import kinesis
+
+from flask_httpauth import HTTPBasicAuth
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
+# initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_security'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+
+STREAM = "ApiStream"
+
+
+kinesis = boto3.client('kinesis')
+
+dynamodb = boto3.resource('dynamodb')
+
+
+Industries = [ {'DeviceId' : '1 ', 'FlowLevel' : '20' , 'FlowRate':'15' , 'IndustryId':'1' , 'Status':'ON' , 'UnitId':'1' , 'WaterFlowId':'W1'}]
+
+names = [{'DeviceId':'1' , 'DeviceName':'flowmeter1' ,'UnitId':'1' , 'UnitName':'jewelry' , 'IndustryId':'1' , 'IndustryName':'cisco' , 'Status':'ON'}]
+
+
+#@app.route('/')
+#def index():
+#    return " This is FLask Application to post the data onto kinesis DataStream !!"
+
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+# api to add new users with username and password
+@app.route('/v1/Aquagen/new_user', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)    # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+#api to obtain the username with userid
+@app.route('/v1/Aquagen/users/<int:id>')
+@auth.login_required
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+# api to obtain a token for authorized users
+@app.route('/v1/Aquagen/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+# api to print any particular user
+@app.route('/v1/Aquagen/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
+
+
+# api to fetch the details of data
+@app.route('/v1/Aquagen/details' , methods =['GET'])
+@auth.login_required
+def get_details():
+    #print({"data":'Hello,%s!' %g.user.username})
+    return jsonify({'list_of_data' : Industries})
+
+#api to post the data
+@app.route('/v1/Aquagen' , methods = ['GET', 'POST'])
+@auth.login_required
+def new_data():
+    Industry_add = {'DeviceId' :request.json['DeviceId'],'FlowLevel':request.json['FlowLevel'] , 'FlowRate' :request.json['FlowRate'] ,
+                    'IndustryId' : request.json['IndustryId'] , 'Status' :request.json['Status'] ,
+                    'UnitId':request.json['UnitId'],'WaterFlowId':request.json['WaterFlowId']}
+
+    Industries.append(Industry_add)
+    kinesis.put_record(StreamName="ApiStream", Data=json.dumps(Industry_add), PartitionKey="filler")
+
+    return jsonify({'Displaying_Industries' : Industry_add})
+
+
+@app.route('/v1/Aquagen/names', methods=['GET', 'POST'])
+def names_db():
+    names_add = {'DeviceId': request.json['DeviceId'], 'DeviceName': request.json['DeviceName'],
+                 'UnitId': request.json['UnitId'],
+                 'UnitName': request.json['UnitName'], 'IndustryId': request.json['IndustryId'],
+                 'IndustryName': request.json['IndustryName'],
+                 'Status': request.json['Status']}
+
+    names.append(names_add)
+
+    table1 = dynamodb.Table('Device')
+    table2 = dynamodb.Table('Industry')
+    table3 = dynamodb.Table('Unit')
+
+    for r in names_add:
+        response = table1.put_item(
+            Item={
+                'DeviceId': names_add['DeviceId'],
+                'IndustryId': names_add['IndustryId'],
+                'Status': names_add['Status'],
+                'UnitId': names_add['UnitId'],
+                'DeviceName': names_add['DeviceName'],
+            }
+        ),
+        response = table2.put_item(
+            Item={
+                'IndustryId': names_add['IndustryId'],
+                'IndustryName': names_add['IndustryName'],
+            }
+        ),
+        response = table3.put_item(
+            Item={
+                'IndustryId': names_add['IndustryId'],
+                'UnitId': names_add['UnitId'],
+                'UnitName': names_add['UnitName']
+            }
+        )
+
+        print(response)
+        print("successfully inserted")
+    return jsonify({'Displaying_names': names_add})
+
+
+if __name__ == '__main__':
+    if not os.path.exists('users_security'):
+        db.create_all()
+    app.run(debug=True,port=5001)
